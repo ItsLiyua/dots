ccdbc440ee6f0eb3c5b431133423694685e4381c
Liyua <liyua@duck.com>
2024-08-06 06:45:43 +0200

    Added even more nvim plugins

--- .config/nvim/lua/liyua/plugins/auto-session.lua
+++ .config/nvim/lua/liyua/plugins/auto-session.lua
@@ -18,5 +18,7 @@ return {
 
     keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
     keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
+
+    vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
   end,
 }
