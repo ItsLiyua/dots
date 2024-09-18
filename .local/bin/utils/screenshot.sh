c2f646092d22835509f6fb1ebec418f24941af3a
Liyua <liyua@duck.com>
2024-09-18 14:51:24 +0200

    Added a test to only allow one instance of the screenshot tool at a time

--- .local/bin/utils/screenshot.sh
+++ .local/bin/utils/screenshot.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 mkdir ~/Pictures/screenshots -p
-slurp | grim -t png -g - "$HOME/Pictures/screenshots/$(date +%Y-%m-%d_%H_%M_%S).png" | wl-copy
+if [ ! -f "/tmp/screenshot_state" ]; then
+  touch /tmp/screenshot_state
+  slurp | grim -t png -g - "$HOME/Pictures/screenshots/$(date +%Y-%m-%d_%H_%M_%S).png" | wl-copy
+  rm /tmp/screenshot_state
+fi
