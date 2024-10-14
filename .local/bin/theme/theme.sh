71dc3a1cbb66f6a0bfec6cca1e6d728beef0a066
Liyua <liyua@duck.com>
2024-10-15 00:43:11 +0200

    Updated discord theme

--- .local/bin/theme/theme.sh
+++ .local/bin/theme/theme.sh
@@ -10,7 +10,7 @@ fi
 ~/.local/bin/theme/swww.sh "$wallpaper" > /dev/null &
 
 # Colorscheme
-wal -t -i "$wallpaper" > /dev/null
+wal -t -i "$wallpaper"
 
 ~/.local/bin/theme/waybar.sh > /dev/null &
 ~/.local/bin/theme/kvantum.sh > /dev/null &
@@ -18,7 +18,7 @@ wal -t -i "$wallpaper" > /dev/null
 ~/.local/bin/theme/mako.sh > /dev/null &
 ~/.local/bin/theme/hyprland.sh > /dev/null &
 ~/.local/bin/theme/pywalfox.sh > /dev/null &
-~/.local/bin/theme/discord.sh > /dev/null &
+~/.local/bin/theme/discord.sh &
 ~/.local/bin/theme/icons.py > /dev/null &
 # ~/.local/bin/theme/btop.sh > /dev/null & # Replaced with symlink
 
