2df6eb66e26994214c67204fee03a33f3048e9a1
Liyua <liyua@duck.com>
2024-12-15 19:18:43 +0100

    Added hyprpolkitagent

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-doas pacman -S btop cmake cpio fzf npm rocm-smi-lib ttf-jetbrains-mono-nerd zsh zoxide
+doas pacman -Syu
+doas pacman -S base-devel
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+doas yay -S \
+  btop rocm-smi-lib \
+  cmake cpio hyprland-git hyprpolkitagent-git xdg-desktop-portal-hyprland-git \
+  fzf npm ttf-jetbrains-mono-nerd zsh zoxide
 
 hyprpm update
 hyprpm add https://github.com/Duckonaut/split-monitor-workspaces -v
