a83d4b536ab7f24a1cefa72c4a80c85cd22369c2
Liyua <liyua@duck.com>
2024-12-15 18:33:01 +0100

    Added hyprland config

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-pacman -S fzf npm ttf-jetbrains-mono-nerd zsh zoxide
+doas pacman -S btop cmake cpio fzf npm rocm-smi-lib ttf-jetbrains-mono-nerd zsh zoxide
+
+hyprpm update
+hyprpm add https://github.com/Duckonaut/split-monitor-workspaces -v
+hyprpm enable split-monitor-workspaces -v
+hyprpm reload
