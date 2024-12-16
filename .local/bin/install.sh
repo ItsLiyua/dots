8f04396d511cad540b20d9c8d880a1c007ff6046
Liyua <liyua@duck.com>
2024-12-16 01:46:50 +0100

    Added hyprland theme

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-doas pacman -Syu
-doas pacman -S base-devel
-
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
+# doas pacman -Syu
+# doas pacman -S base-devel
+#
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si
+#
 yay -S \
   btop rocm-smi-lib \
   ly cmake cpio hyprland-git hyprpolkitagent-git xdg-desktop-portal-hyprland-git \
@@ -15,7 +15,7 @@ yay -S \
   kitty fzf npm ttf-jetbrains-mono-nerd zsh zoxide \
   nautilus-share nautilus-image-converter nautilus
 
-systemctl enable ly
+# systemctl enable ly
 
 # hyprpm update
 # hyprpm add https://github.com/Duckonaut/split-monitor-workspaces -v
