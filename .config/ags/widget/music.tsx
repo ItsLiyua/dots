99e10b607754cb69dbe4a74df797b86cbc9b8e49
Liyua <liyua@duck.com>
2024-12-28 23:20:31 +0100

    Fixed a bug that prevented ags from launching

--- .config/ags/widget/music.tsx
+++ .config/ags/widget/music.tsx
@@ -1,44 +1,48 @@
-import { bind } from "astal"
+import { bind, Variable } from "astal"
 import { Gtk } from "astal/gtk3"
 import Mpris from "gi://AstalMpris?version=0.1"
+import { Nothing } from "./utils/nothing"
 
 const mpris = Mpris.get_default()
 const players = bind(mpris, "players")
 const maxTextLength = 20
 
-function trim(s: string): string {
-  if (s.length > maxTextLength) return s.substring(0, maxTextLength) + "..."
+function trim(s: string | null): string {
+  if (s == null) return ""
+  else if (s.length > maxTextLength) return s.substring(0, maxTextLength) + "..."
   else return s
 }
 
 export function Music(): JSX.Element {
-  return players.as(ps => {
-    if (ps.length > 0) {
-      const p = ps[0]
-      const title = bind(p, "title")
-      const artist = bind(p, "artist")
-      const paused = bind(p, "playbackStatus")
+  const p = players.get()[0]
+  const title = bind(p, "title")
+  const artist = bind(p, "artist")
+  const paused = bind(p, "playbackStatus")
+  const derived = Variable.derive([title, artist, paused])
 
-      return <box>
-        <box vertical={true}>
-          <label halign={Gtk.Align.START} label={title.as(s => trim(s))} />
-          <label halign={Gtk.Align.START} label={artist.as(s => trim(s))} />
-        </box>
-        <box>
-          <button onClick={() => p.previous()}>
-            <label className="icon" label="" />
-          </button>
-          <button onClick={() => {
-            if (p.playbackStatus == Mpris.PlaybackStatus.PAUSED && p.canPlay) p.play()
-            else if (p.playbackStatus == Mpris.PlaybackStatus.PLAYING && p.canPause) p.pause()
-          }}>
-            <label className="icon" label={paused.as(p => p ? "" : "")} />
-          </button>
-          <button onClick={() => { if (p.can_go_previous) p.previous() }}>
-            <label className="icon" label="" />
-          </button>
-        </box>
+  return bind(derived).as(n => {
+    if (n[0] == null) return Nothing()
+    else if (n[1] == null) return Nothing()
+    else if (n[2] == Mpris.PlaybackStatus.STOPPED) return Nothing()
+    else return <box>
+      <box vertical={true}>
+        <label halign={Gtk.Align.START} label={trim(n[0])} />
+        <label halign={Gtk.Align.START} label={trim(n[1])} />
       </box>
-    } else return <box />
-  })
-}
+      <box>
+        <button onClick={() => p.previous()}>
+          <label className="icon" label="" />
+        </button>
+        <button onClick={() => {
+          if (n[2] == Mpris.PlaybackStatus.PAUSED && p.canPlay) p.play()
+          else if (n[2] == Mpris.PlaybackStatus.PLAYING && p.canPause) p.pause()
+        }}>
+          <label className="icon" label={paused.as(p => p ? "" : "")} />
+        </button>
+        <button onClick={() => { if (p.can_go_previous) p.previous() }}>
+          <label className="icon" label="" />
+        </button>
+      </box>
+    </box>
+  }).get()
+} // TODO: Fix bug that prevents widget from updating
