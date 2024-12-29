abaa81edbb3922308d576420569730828c3d16fd
Liyua <liyua@duck.com>
2024-12-29 21:18:02 +0100

    Did some styling on the sysmenu widget

--- .config/ags/widget/window/SystemMenuWindow.tsx
+++ .config/ags/widget/window/SystemMenuWindow.tsx
@@ -1,6 +1,7 @@
 import { Variable } from "astal";
 import { bind, Subscribable } from "astal/binding";
-import { Astal, Gdk } from "astal/gtk3";
+import { Astal, Gdk, Gtk } from "astal/gtk3";
+import style from "../../styles/sysmenu.scss"
 
 const windows = new Map<Gdk.Monitor, any>()
 
@@ -11,7 +12,7 @@ export function toggleSysWindow(gdkmonitor: Gdk.Monitor) {
 
 function MenuButton({ icon, onToggle, onMenuOpen, initState = false }: { icon: Subscribable<string>, onToggle: (state: boolean) => boolean, onMenuOpen: () => void, initState: boolean }): JSX.Element {
   const state = Variable(initState)
-  return <box className={bind(state).as(s => s ? "active" : "")}>
+  return <box className={bind(state).as(s => s ? "active sysmenu test" : "sysmenu")}>
     <button onClick={() => state.set(onToggle(state.get()))}>
       <label label={bind(icon)} />
     </button>
@@ -23,9 +24,9 @@ function MenuButton({ icon, onToggle, onMenuOpen, initState = false }: { icon: S
 
 function Switch({ icon, onToggle, initState }: { icon: Subscribable<string>, onToggle: (state: boolean) => boolean, initState: boolean }): JSX.Element {
   const state = Variable(initState)
-  return <button className={bind(state).as(s => s ? "active" : "")} onClick={() => state.set(onToggle(state.get()))}>
+  return <button className={bind(state).as(s => s ? "active sysmenu sysmenuelement" : "sysmenu sysmenuelement")}
+    onClick={() => state.set(onToggle(state.get()))}>
     <label label={bind(icon)} />
-
   </button>
 }
 
@@ -43,32 +44,35 @@ export function SysMenuWindow(gdkmonitor: Gdk.Monitor) {
     visible={true}
     keymode={Astal.Keymode.ON_DEMAND}
     gdkmonitor={gdkmonitor}
+    css={style}
     onKeyPressEvent={(self, e) => { if (e.get_keyval()[1] == Gdk.KEY_Escape) self.hide() }}
     setup={self => windows.set(gdkmonitor, self)}
   >
-    <box className="sysmenubox" vertical={true}>
+    <box className="sysmenubox sysmenu" vertical={true} halign={Gtk.Align.CENTER}>
       <label label="System Menu" />
       <box vertical={false}>
         <MenuButton icon={Variable("wifi")} onToggle={b => !b} onMenuOpen={() => { }} initState={false} />
         <MenuButton icon={Variable("bluetooth")} onToggle={b => !b} onMenuOpen={() => { }} initState={true} />
       </box>
-      <box className="bat" vertical={false}>
+      <box className="bat sysmenu" vertical={false} halign={Gtk.Align.CENTER}>
         <label label="baticon" />
         <label label="percentage" />
       </box>
-      <box className="vol" vertical={false}>
+      <box className="vol sysmenu" vertical={false} halign={Gtk.Align.CENTER}>
+        <label label="vol" />
         <slider />
       </box>
-      <box className="brightness" vertical={false}>
+      <box className="brightness sysmenu" vertical={false} halign={Gtk.Align.CENTER}>
+        <label label="brt" />
         <slider />
       </box>
-      <box className="controls" vertical={true}>
-        <box className="controlrow" vertical={false}>
+      <box className="controls sysmenu" vertical={true}>
+        <box className="controlrow sysmenu" vertical={false} halign={Gtk.Align.CENTER}>
           <Switch icon={Variable("1")} onToggle={b => !b} initState={false} />
           <Switch icon={Variable("2")} onToggle={b => !b} initState={false} />
           <Switch icon={Variable("3")} onToggle={b => !b} initState={false} />
         </box>
-        <box className="controlrow" vertical={false}>
+        <box className="controlrow sysmenu" vertical={false} halign={Gtk.Align.CENTER}>
           <Switch icon={Variable("4")} onToggle={b => !b} initState={false} />
           <Switch icon={Variable("5")} onToggle={b => !b} initState={false} />
           <Switch icon={Variable("6")} onToggle={b => !b} initState={false} />
