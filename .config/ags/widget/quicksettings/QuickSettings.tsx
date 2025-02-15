e93eb1b30f6977a1e256511e5c6c8cbe5b34de36
Liyua <liyua@duck.com>
2025-02-15 13:39:59 +0100

    Added layout for quicksettings menu

--- .config/ags/widget/quicksettings/QuickSettings.tsx
+++ .config/ags/widget/quicksettings/QuickSettings.tsx
@@ -1,4 +1,4 @@
-import { App, Astal } from "astal/gtk3";
+import { App, Astal, Gtk } from "astal/gtk3";
 import Wifi from "./buttons/Wifi";
 
 export const QUICK_SETTINGS_NAME = "QuickSettings";
@@ -17,7 +17,52 @@ export default function QuickSettings(monitor: number) {
       layer={Astal.Layer.TOP}
       visible={false}
     >
-      <Wifi />
+      <box orientation={Gtk.Orientation.VERTICAL}>
+        <box orientation={Gtk.Orientation.HORIZONTAL}>
+          <box orientation={Gtk.Orientation.HORIZONTAL}>
+            <button>
+              <label label="toggle 1" />
+            </button>
+            <button>
+              <label label="opts 1" />
+            </button>
+          </box>
+          <box orientation={Gtk.Orientation.HORIZONTAL}>
+            <button>
+              <label label="toggle 2" />
+            </button>
+            <button>
+              <label label="opts 2" />
+            </button>
+          </box>
+        </box>
+        <box orientation={Gtk.Orientation.VERTICAL}>
+          <box orientation={Gtk.Orientation.HORIZONTAL}>
+            <label label="icon1" />
+            <slider />
+          </box>
+          <box orientation={Gtk.Orientation.HORIZONTAL}>
+            <label label="icon1" />
+            <slider />
+          </box>
+        </box>
+        <box orientation={Gtk.Orientation.VERTICAL}>
+          <box orientation={Gtk.Orientation.HORIZONTAL}>
+            <button label="button 1" />
+            <button label="button 2" />
+            <button label="button 3" />
+          </box>
+          <box orientation={Gtk.Orientation.HORIZONTAL}>
+            <button label="button 4" />
+            <button label="button 5" />
+            <button label="button 6" />
+          </box>
+        </box>
+        <box orientation={Gtk.Orientation.HORIZONTAL}>
+          <label label="baticon" />
+          <label label="batpercentage" />
+        </box>
+      </box>
     </window>
   );
 }
