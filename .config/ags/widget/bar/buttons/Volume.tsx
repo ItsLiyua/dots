cda5b7edb4afee20208fd368f8ef5d8856e90e73
Liyua <liyua@duck.com>
2025-04-05 22:07:18 +0200

    Fixed volume widget

--- .config/ags/widget/bar/buttons/Volume.tsx
+++ .config/ags/widget/bar/buttons/Volume.tsx
@@ -1,41 +1,49 @@
-import { bind, derive } from "astal";
+import { bind, Binding, derive } from "astal";
 import Wp from "gi://AstalWp";
+import HoverIcon from "../HoverIcon";
 
 const wp = Wp.get_default()!!;
 const default_audio = bind(wp.audio, "default_speaker");
 const MUTE_ICON = "";
 const ICONS = ["", "", ""];
-const STEP = 0.01;
+const STEP = 0.05;
 
-function icon(vol: number, mute: boolean) {
-  if (mute) return MUTE_ICON;
+let vol: Binding<number> = bind(default_audio.get(), "volume");
+default_audio.subscribe((s) => (vol = bind(s, "volume")));
 
-  const percentagePerStep = 1.0 / ICONS.length;
+let mute: Binding<boolean> = bind(default_audio.get(), "mute");
+default_audio.subscribe((s) => (mute = bind(s, "mute")));
+
+function icon(vol: number) {
+  if (vol <= 0) return MUTE_ICON;
   for (let i = 0; i < ICONS.length; i++) {
-    const check = (i + 1) * percentagePerStep;
-    if (vol <= check) {
-      return ICONS[i];
-    }
+    const icon = ICONS[i];
+    if (vol <= (i + 1) * (100 / ICONS.length)) return icon;
   }
-
-  return "undefined";
+  return ICONS[ICONS.length - 1];
 }
 
 export default function Volume() {
   return (
-    <eventbox
-      onClick={() => (default_audio.get().mute = !default_audio.get().mute)}
-      onScroll={(_, e) => {
-        if (e.delta_y > 0) console.log("test2");
-        else if (e.delta_y < 0) console.log("test3");
+    <HoverIcon
+      iconProvider={(value: number): string => icon(value)}
+      valueProvider={bind(derive([vol, mute])).as((v) => (v[1] ? 0 : v[0]))}
+      onScrollUp={() => {
+        default_audio.get().volume = Math.min(
+          default_audio.get().volume + STEP,
+          1,
+        );
+        default_audio.get().mute = false;
       }}
-    >
-      <label
-        css="margin-right: 8px;"
-        label={default_audio
-          .as((a) => bind(a, "volume").as((n) => "" + n))
-          .get()}
-      />
-    </eventbox>
+      onScrollDown={() => {
+        default_audio.get().volume = Math.max(
+          default_audio.get().volume - STEP,
+          0,
+        );
+        default_audio.get().mute = default_audio.get().volume == 0;
+      }}
+      onClick={(_) => (default_audio.get().mute = !default_audio.get().mute)}
+      initState={false}
+    />
   );
 }
