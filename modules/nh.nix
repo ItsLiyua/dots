c0f496c0ab23948533ca66c7eb965ac4b2ebbfc0
Liyua <liyua@duck.com>
2025-05-16 03:22:11 +0200

    Split nh flake definition into userspace and system flake

--- modules/nh.nix
+++ modules/nh.nix
@@ -1,9 +1,11 @@
 { config, pkgs, ... }: {
-	# environment.sessionVariables.NH_FLAKE = "/etc/nixos";
+	environment.sessionVariables = {
+		NH_OS_FLAKE = "/etc/nixos";
+		NH_HOME_FLAKE = ".config/home-manager";
+	};
 	# environment.systemPackages = [ pkgs.nh ];
 	programs.nh = {
 		enable = true;
 		clean.enable = false;
-		flake = "/etc/nixos";
 	};
 }
