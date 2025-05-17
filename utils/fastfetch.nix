b3259bdfaac840ffcd3bf5e2a56a353c72c08459
Liyua <liyua@duck.com>
2025-05-17 19:47:03 +0200

    Configured fastfetch

--- utils/fastfetch.nix
+++ utils/fastfetch.nix
@@ -2,7 +2,46 @@
 	programs.fastfetch = {
 		enable = true;
 		settings = {
-			
+			logo = {
+				source = "nixos_small";
+				padding.right = 4;
+			};
+			display = {
+				size.binaryPrefix = "si";
+				color = {
+					keys = "blue";
+					title = "red";
+				};
+				key = {
+					width = 10;
+					type = "string";
+				};
+				percent = {
+					type = 9;
+					color = {
+						green = "green";
+						yellow = "light_yellow";
+						red = "light_red";
+					};
+				};
+				separator = " ";
+			};
+
+			modules = [
+				"title"
+				"os"
+				# {
+				# 	type = "os";
+				# 	key = "OS";
+				# 	keyColor = "blue";
+				# 	format = "{name} {version}";
+				# }
+				"host"
+				"kernel"
+				"shell"
+				"uptime"
+				"packages"
+			];
 		};
 	};
 }
