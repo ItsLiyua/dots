8ac3fcdc7345d46475b6dfcd9fbb92c4e785e4c4
Liyua <liyua@duck.com>
2025-05-18 00:27:25 +0200

    Added some basic nvim config

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -5,5 +5,36 @@
 		defaultEditor = true;
 		viAlias = true;
 		vimAlias = true;
+		configure.customRC = ''
+set autoindent
+set tabstop=2
+set softtabstop=2
+set expandtab
+
+set number
+set relativenumber
+
+set wrap!
+
+set ignorecase
+set smartcase
+
+set termguicolors
+set background=dark
+set signcolumn=yes
+set backspace=indent,eol,start
+set clipboard=unnamedplus
+
+set splitright
+set splitbelow
+
+set conceallevel=2
+
+set scrolloff=10
+set sidescrolloff=20
+
+set cursorline
+set autoread
+		'';
 	};
 }
