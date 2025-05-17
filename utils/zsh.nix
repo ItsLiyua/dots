e73559a957a2b0ddf89190aee986475e5498d34a
Liyua <liyua@duck.com>
2025-05-17 20:40:16 +0200

    Removed fzf because it made my shell slow

--- utils/zsh.nix
+++ utils/zsh.nix
@@ -17,37 +17,8 @@
 			v = "nvim";
 			cd = "z";
 		};
-		initContent = ''
-export ZSH_COMPDUMP=$HOME/.cache/zsh/zcompdump-$HOST
-
-ZINIT_HOME="''\${XDG_DATA_HOME:-''\${HOME}/.local/share}/zinit/zinit.git"
-[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
-[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
-source "''\${ZINIT_HOME}/zinit.zsh"
-
-autoload -Uz compinit && compinit
-
-zinit cdreplay -q
-
-zinit light Aloxaf/fzf-tab
-
-zinit snippet OMZL::git.zsh
-zinit snippet OMZP::git
-
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-zstyle ':completion:*' list-colors "''\${(s.:.)LS_COLORS}"
-zstyle ':completion:*' menu no
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
-zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
-
-fastfetch
-		'';
 	};
 	programs.zoxide = {
-		enable = true;
-		enableZshIntegration = true;
-	};
-	programs.fzf = {
 		enable = true;
 		enableZshIntegration = true;
 	};
