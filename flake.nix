f705516fd2058fe0e6307956387243e2804049e5
Liyua <liyua@duck.com>
2025-05-19 09:56:58 +0200

    Added stylix and updated system

--- flake.nix
+++ flake.nix
@@ -6,8 +6,12 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = inputs @ { nixpkgs, disko, ... }: 
+  outputs = inputs @ { nixpkgs, disko, stylix, ... }: 
   let 
     system = "x86_64-linux";
   in {
@@ -15,9 +19,10 @@
       inherit system;
       specialArgs = { inherit inputs; };
       modules = [ 
-	./shared.nix
-        ./liberty/configuration.nix 
         disko.nixosModules.disko
+        stylix.nixosModules.stylix
+        ./shared.nix
+        ./liberty/configuration.nix 
       ];
     };
   };
