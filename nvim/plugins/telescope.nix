d0b72a1a8a290a9d613cecc0448772522407feb9
Liyua <liyua@duck.com>
2025-05-20 22:01:23 +0200

    Added C-J and C-K to telescope

--- nvim/plugins/telescope.nix
+++ nvim/plugins/telescope.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }: {
+  home.packages = [ pkgs.ripgrep ];
   programs.nixvim = {
     extraPlugins = [ pkgs.vimPlugins.smart-open-nvim ];
     plugins.telescope = {
@@ -7,15 +8,16 @@
         ui-select.enable = true;
         fzf-native.enable = true;
       };
+      enabledExtensions = [ "smart_open" ];
       settings = {
-        enabledExtensions = [ "smart_open" ];
-        mappings = {
-          i = let
-            action = m: ''require("telescope.actions").${m}'';
-          in {
-            "<C-k>".__raw = action "move_selection_previous";
-            "<C-j>".__raw = action "move_selection_next";
-            "<C-q>".__raw = action "close";
+        defaults = {
+          mappings = {
+            i = {
+              "<C-K>".__raw = ''require("telescope.actions").move_selection_previous'';
+              "<C-J>".__raw = ''require("telescope.actions").move_selection_next'';
+              "<C-q>".__raw = ''require("telescope.actions").close'';
+              "<Esc>".__raw = ''require("telescope.actions").close'';
+            };
           };
         };
       };
