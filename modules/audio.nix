e246c45525365d54963f06cb6f6aa9a59b6573b4
Liyua <liyua@duck.com>
2025-05-20 23:53:36 +0200

    Added pipewire

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,6 +1,11 @@
 { config, lib, pkgs, ... }: {
+  security.rtkit.enable = true;
 	services.pipewire = {
 		enable = true;
+    audio.enable = true;
+    jack.enable = true;
+    alsa.enable = true;
 		pulse.enable = true;
+    wireplumber.enable = true;
 	};
 }
