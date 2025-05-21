b75b0f527751799211774ca5ce768625c9fa1531
Liyua <liyua@duck.com>
2025-05-21 15:26:26 +0200

    Added cache file with colors for ags

--- desktop/ags/ags.nix
+++ desktop/ags/ags.nix
@@ -13,4 +13,13 @@
     Install.WantedBy = [ "graphical-session.target" ];
     Service.ExecStart = "${inputs.ags.packages.${pkgs.system}.default}/bin/ags run --gtk4";
   };
+  home.file.".cache/ags/colors.scss".text = 
+  lib.strings.concatMapStrings
+  (x: "${x}: ${config.lib.stylix.colors.withHashtag.${x}};\n") 
+  (map 
+    (x: "base0${x}") 
+    ((map 
+      (x: toString x) 
+      (lib.range 0 9)
+    ) ++ [ "A" "B" "C" "D" "E" "F" ]));
 }
