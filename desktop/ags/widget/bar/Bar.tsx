be5c35e338e8d6a86e3cc2a463256f2379561958
Liyua <liyua@duck.com>
2025-05-22 02:30:30 +0200

    Added bar layout

--- desktop/ags/widget/bar/Bar.tsx
+++ desktop/ags/widget/bar/Bar.tsx
@@ -1,7 +1,4 @@
-import { App, Astal, Gtk, Gdk } from "astal/gtk4";
-import { Variable } from "astal";
-
-const time = Variable("").poll(1000, "date");
+import { App, Astal, Gdk } from "astal/gtk4";
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
   const { TOP, LEFT, RIGHT } = Astal.WindowAnchor;
@@ -17,13 +14,19 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
     >
       <centerbox>
         <box cssName="start">
-          <label label="Start" />
+          <label label="Launcher" />
+          <label label="Workspace" />
+          <label label="Audio" />
         </box>
         <box cssName="center">
-          <label label="Center" />
+          <label label="Title" />
         </box>
         <box cssName="end">
-          <label label="End" />
+          <label label="Screenshot/Colorpicker" />
+          <label label="Mic/Vol/Brightness" />
+          <label label="Wifi/Bt/Bat" />
+          <label label="Clock" />
+          <label label="Power" />
         </box>
       </centerbox>
     </window>
