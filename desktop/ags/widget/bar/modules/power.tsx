47d685d0d08aa000d69e8314999fb3d5bb31de19
Liyua <liyua@duck.com>
2025-05-24 00:11:34 +0200

    Added power widget

--- desktop/ags/widget/bar/modules/power.tsx
+++ desktop/ags/widget/bar/modules/power.tsx
@@ -1,7 +1,36 @@
+import { bind, exec, Variable } from "astal";
+import { Gtk } from "astal/gtk4";
+
 export default function Power() {
+  const reveal = Variable(false);
   return (
-    <box cssClasses={["element", "power"]}>
-      <label label="Power" />
+    <box
+      cssClasses={["element", "power"]}
+      onHoverEnter={() => reveal.set(true)}
+      onHoverLeave={() => reveal.set(false)}
+    >
+      <revealer
+        revealChild={bind(reveal)}
+        transitionType={Gtk.RevealerTransitionType.SLIDE_LEFT}
+        transitionDuration={500}
+      >
+        <box>
+          <button onClicked={() => exec(["bash", "-c", "hyprlock"])}>
+            <label cssClasses={["icon"]} label="" />
+          </button>
+          <button
+            onClicked={() => exec(["bash", "-c", "hyprctl dispatch exit 0"])}
+          >
+            <label cssClasses={["icon"]} label="󰍃" />
+          </button>
+          <button onClicked={() => exec(["bash", "-c", "doas reboot now"])}>
+            <label cssClasses={["icon"]} label="" />
+          </button>
+        </box>
+      </revealer>
+      <button onClicked={() => exec(["bash", "-c", "doas shutdown now"])}>
+        <label cssClasses={["icon"]} halign={Gtk.Align.CENTER} label="" />
+      </button>
     </box>
   );
 }
