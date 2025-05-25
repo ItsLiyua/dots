dd2beb96257ad739179eb57d7d22cb2948125c0a
Liyua <liyua@duck.com>
2025-05-25 23:28:27 +0200

    Rewrote logic whether workspace should be displayed

--- desktop/ags/widget/bar/modules/workspaces.tsx
+++ desktop/ags/widget/bar/modules/workspaces.tsx
@@ -15,22 +15,31 @@ function workspaceButton(ws: number) {
   return (
     <box
       cssClasses={
-        hyprland.focusedWorkspace.id == ws ? ["button", "active"] : ["button"]
+        hyprland.focusedWorkspace.id == ws + 1
+          ? ["button", "active"]
+          : ["button"]
       }
     >
-      <button onClicked={() => hyprland.dispatch("workspace", "" + ws)}>
-        <label label={"" + (ws % 10 == 0 ? 10 : ws % 10)} />
+      <button onClicked={() => hyprland.dispatch("workspace", "" + (ws + 1))}>
+        <label label={"" + (ws + 1)} />
       </button>
     </box>
   );
 }
 
-function showWorkspace(mon: Hyprland.Monitor, ws: number) {
-  const w = hyprland.get_workspace(ws);
-  if (ws < 0) return false;
-  if (w != null && w.monitor.id != mon.id) return false;
-  if (ws % 10 != 0 && ws % 10 < MIN_WS_PER_MON + 1) return true;
-  if (hyprland.focusedWorkspace.id >= ws) return true;
+function showWorkspace(mon: number, ws: number) {
+  const range = [mon * WS_PER_MON, (mon + 1) * WS_PER_MON];
+  if (ws < range[0] || ws >= range[1]) return false;
+  else if (ws % 10 < MIN_WS_PER_MON) return true;
+  else if (ws <= hyprland.focusedWorkspace.id - 1) return true;
+  else if (
+    [...Array(WS_PER_MON).keys()]
+      .map((n) => mon * WS_PER_MON + n)
+      .filter((n) => n >= ws)
+      .map((n) => hyprland.get_workspace(n + 1))
+      .filter((ws) => ws != null && ws.clients.length > 0).length > 0
+  )
+    return true;
   return false;
 }
 
@@ -56,9 +65,9 @@ export default function Workspaces({
         ]),
       ).as((_) =>
         [...Array(WS_PER_MON).keys()]
-          .map((n) => n + 1)
+          .map((n) => n)
           .map((n) => mon.id * WS_PER_MON + n)
-          .filter((n) => showWorkspace(mon, n))
+          .filter((n) => showWorkspace(mon.id, n))
           .sort((a, b) => a - b)
           .map((n) => workspaceButton(n)),
       )}
