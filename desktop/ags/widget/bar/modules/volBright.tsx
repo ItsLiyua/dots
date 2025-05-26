b4bf4ed67da59c11321fb3971b69059b6460e615
Liyua <liyua@duck.com>
2025-05-26 13:35:30 +0200

    Added brightness widget

--- desktop/ags/widget/bar/modules/volBright.tsx
+++ desktop/ags/widget/bar/modules/volBright.tsx
@@ -1,7 +1,117 @@
+import { bind, exec, execAsync, Variable } from "astal";
+import { Gtk } from "astal/gtk4";
+import Wp from "gi://AstalWp?version=0.1";
+
+const wp = Wp.get_default()!!;
+
+const BRIGHTNESS_STEP = 0.01;
+const VOLUME_STEP = 0.03;
+const BRIGHTNESS_ICONS = [
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+];
+const MAX_BRIGHTNESS = parseInt(
+  exec(["bash", "-c", "brightnessctl -c backlight m"]),
+);
+const CURRENT_BRIGHTNESS_POLL_CMD = [
+  "bash",
+  "-c",
+  "brightnessctl -c backlight g",
+];
+const CURRENT_BRIGHTNESS_POLL_PARSER = (q: string) =>
+  parseFloat(q) / MAX_BRIGHTNESS;
+const currentBrightness = Variable(0).poll(
+  1000,
+  CURRENT_BRIGHTNESS_POLL_CMD,
+  CURRENT_BRIGHTNESS_POLL_PARSER,
+);
+
+const expandBrightness = Variable(false);
+const expandVolume = Variable(false);
+const expandMicVolume = Variable(false);
+const fastPollBrightness = Variable(false);
+fastPollBrightness.subscribe((s) => {
+  if (currentBrightness.isPolling()) currentBrightness.stopPoll();
+  if (s)
+    currentBrightness.poll(
+      50,
+      CURRENT_BRIGHTNESS_POLL_CMD,
+      CURRENT_BRIGHTNESS_POLL_PARSER,
+    );
+  else
+    currentBrightness.poll(
+      1000,
+      CURRENT_BRIGHTNESS_POLL_CMD,
+      CURRENT_BRIGHTNESS_POLL_PARSER,
+    );
+});
+
+function icon(icons: string[], value: number): string {
+  for (let i = 0; i < icons.length; i++)
+    if ((1.0 / icons.length) * i >= value) return icons[i];
+  return icons[icons.length - 1];
+}
+
+function brightnessUp() {
+  execAsync(["bash", "-c", "brightnessctl s +" + BRIGHTNESS_STEP * 100 + "%"]);
+}
+
+function brightnessDown() {
+  execAsync(["bash", "-c", "brightnessctl s " + BRIGHTNESS_STEP * 100 + "%-"]);
+}
+
 export default function VolumeBrightness() {
   return (
     <box cssClasses={["element", "volBright"]}>
-      <label label="VolBright" />
+      <box
+        cssClasses={["volume"]}
+        onHoverEnter={() => expandVolume.set(true)}
+        onHoverLeave={() => expandVolume.set(false)}
+      ></box>
+      <box
+        cssClasses={["micVolume"]}
+        onHoverEnter={() => expandMicVolume.set(true)}
+        onHoverLeave={() => expandMicVolume.set(false)}
+      ></box>
+      <box
+        cssClasses={["brightness"]}
+        onScroll={(_, __, dy) => {
+          if (dy < 0) brightnessUp();
+          else if (dy > 0) brightnessDown();
+        }}
+        onHoverEnter={() => {
+          expandBrightness.set(true);
+          fastPollBrightness.set(true);
+        }}
+        onHoverLeave={() => {
+          expandBrightness.set(false);
+          fastPollBrightness.set(false);
+        }}
+      >
+        <label
+          label={bind(currentBrightness).as((a) => icon(BRIGHTNESS_ICONS, a))}
+        />
+        <revealer
+          revealChild={bind(expandBrightness)}
+          transitionType={Gtk.RevealerTransitionType.SLIDE_LEFT}
+        >
+          <label
+            label={bind(currentBrightness).as((b) => Math.round(b * 100) + "")}
+          />
+        </revealer>
+      </box>
     </box>
   );
 }
