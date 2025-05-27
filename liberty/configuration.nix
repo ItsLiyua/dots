4780a22b6ec0008eca038af4ed440993c7c58748
Liyua <liyua@duck.com>
2025-05-27 15:58:42 +0200

    Added bluetooth

--- liberty/configuration.nix
+++ liberty/configuration.nix
@@ -1,14 +1,12 @@
-{ config, lib, pkgs, inputs, ... }:
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-      ./modules/disko.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./modules/disko.nix
+    ./modules/bluetooth.nix
+  ];
 
   networking.hostName = "liberty";
   console.keyMap = "de";
 
   system.stateVersion = "24.11"; # DO NOT CHANGE UNDER ANY CIRCUMSTANCES!
 }
-
