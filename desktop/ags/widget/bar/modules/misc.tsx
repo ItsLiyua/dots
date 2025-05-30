880e550d3d9bcb98b33cab53a8040e43afbb7c8f
Liyua <liyua@duck.com>
2025-05-30 02:21:51 +0200

    Added some more code to the screen rotation. Backing up with git before starting to adjust nix config

--- desktop/ags/widget/bar/modules/misc.tsx
+++ desktop/ags/widget/bar/modules/misc.tsx
@@ -1,11 +1,26 @@
-import { execAsync } from "astal";
+import { bind, execAsync, Variable } from "astal";
+import AstalHyprland from "gi://AstalHyprland?version=0.1";
+
+enum Rotation {
+  LANDSCAPE = "toPortrait 󰨵",
+  PORTRAIT = "toLandscape 󰹑",
+}
+
+const hyprland = AstalHyprland.get_default();
+const enableRotate = Variable(hyprland.monitors.length == 1);
+const rotateMode = Variable(Rotation.LANDSCAPE);
 
 export default function Misc() {
-  // 󰹑
   return (
-    <box cssClasses={["element", "misc"]}>
-      <button cssClasses={["rotateScreen", "toPortrait"]}>
-        <label label="󰨵" />
+    <box cssClasses={["element", "misc"]} visible={bind(enableRotate)}>
+      <button
+        cssClasses={bind(rotateMode).as((m) => [
+          "rotateScreen",
+          m.split(" ")[0],
+        ])}
+        onClicked={}
+      >
+        <label label={bind(rotateMode).as((m) => m.split(" ")[1])} />
       </button>
       <button
         cssClasses={["osk"]}
