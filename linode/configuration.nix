c2cc60002dfa17a961d8f2d75d00922db353ec05
Liyua <liyua@duck.com>
2025-05-31 21:49:09 +0200

    Disabled all non-mandatory options for linode server

--- linode/configuration.nix
+++ linode/configuration.nix
@@ -1,1 +1,12 @@
-{}
+{
+  config.liyua = {
+    audio.enable = false;
+    extras.dconf.enable = false;
+    fonts.enable = false;
+    gaming.enable = false;
+    greeter.enable = false;
+    libinput.enable = false;
+    logind.enable = false;
+    waylandNativeOzone.enable = false;
+  };
+}
