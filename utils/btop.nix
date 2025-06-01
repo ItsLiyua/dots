f4254da2deebbc1c8a157c92562d21ede3028107
Liyua <liyua@duck.com>
2025-06-01 17:26:32 +0200

    switched btop to btop-rocm to also show a gpu graph

--- utils/btop.nix
+++ utils/btop.nix
@@ -1,11 +1,13 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   options.liyua.cli.btop.enable = lib.mkEnableOption "Btop++";
   config.programs.btop = lib.mkIf config.liyua.cli.btop.enable {
     enable = true;
+    package = pkgs.btop-rocm;
     settings = {
       theme_background = false;
       vim_keys = true;
