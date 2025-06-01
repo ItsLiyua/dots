8612fff318cbb6910d9141dcadbcffbf2923e81b
Liyua <liyua@duck.com>
2025-06-01 16:17:35 +0200

    Backup

--- desktop/hyprland.nix
+++ desktop/hyprland.nix
@@ -158,6 +158,8 @@
       ];
       plugin.split-monitor-workspaces.count = lib.mkIf config.liyua.desktop.hyprland.splitMonitorWorkspaces.enable 10;
     };
-    plugins = lib.mkIf config.liyua.desktop.hyprland.splitMonitorWorkspaces.enable ["${inputs.split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces}/lib/split-monitor-workspaces.so"];
+    plugins =
+      lib.mkIf config.liyua.desktop.hyprland.splitMonitorWorkspaces.enable
+      [inputs.split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces];
   };
 }
