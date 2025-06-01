8e6ddc480b2cf2070728d7a526c591a4e59e37e1
Liyua <liyua@duck.com>
2025-06-01 04:51:11 +0200

    Updated ssh pubkey for liyua on resolute

--- modules/users.nix
+++ modules/users.nix
@@ -18,7 +18,7 @@
         "power"
       ];
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIESk4L3fxgRsPpheIChUPGdaNBnqBrnM/uBISYa5foxh liyua@resolute"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtyKAptgaf6VNiQPw6GvoC8/89u5+PMsQsqa8G7R4J/ liyua@resolute"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKZZyrwb0depJAXqeoN8+q8kquwgAHz3uRssneGDtp0 liyua@liberty"
       ];
     };
