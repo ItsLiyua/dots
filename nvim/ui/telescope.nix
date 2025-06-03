06ccf54e622ec9cdbb79769c117e8ec9137a3a86
Liyua <liyua@duck.com>
2025-06-04 01:48:51 +0200

    Working on telescope config

--- nvim/ui/telescope.nix
+++ nvim/ui/telescope.nix
@@ -1,10 +1,28 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   options.liyua.nvim.ui.telescope.enable = lib.mkEnableOption "Telescope";
   config = {
     liyua.nvim.ui.telescope.enable = lib.mkDefault config.liyua.nvim.ui.enable;
     programs.nvf.settings.vim = lib.mkIf config.liyua.nvim.ui.telescope.enable {
-      telescope.enable = true;
+      telescope = {
+        enable = true;
+      };
+      lazy.plugins = with pkgs.vimPlugins; {
+        # "telescope-ui-select.nvim" = {
+        #   package = telescope-ui-select-nvim;
+        # };
+        # "telescope-fzf-native.nvim" = {
+        #   package = telescope-fzf-native-nvim;
+        # };
+        "smart-open.nvim" = {
+          package = smart-open-nvim;
+        };
+      };
       keymaps = [
         {
           key = "<leader><leader>";
