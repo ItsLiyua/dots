750df2cec64a6a57604e759ddc2117d269c094c5
Liyua <liyua@duck.com>
2025-06-04 17:14:29 +0200

    Moved secrets to another repo so I can access them from within the home cofiguration as well

--- flake.nix
+++ flake.nix
@@ -10,34 +10,41 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
-  outputs = inputs @ {nixpkgs, ...}: let
-    system = "x86_64-linux";
-  in {
-    nixosConfigurations = {
-      liberty = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./shared.nix
-          ./liberty/configuration.nix
-        ];
-      };
-      linode = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./shared.nix
-          ./linode/configuration.nix
-        ];
-      };
-      resolute = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./shared.nix
-          ./resolute/configuration.nix
-        ];
-      };
+    mysecrets = {
+      url = "git+ssh://git@github.com/ItsLiyua/sops-secrets.git?ref=master&shallow=1";
+      flake = false;
     };
   };
+  outputs =
+    inputs@{ nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations = {
+        liberty = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./shared.nix
+            ./liberty/configuration.nix
+          ];
+        };
+        linode = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./shared.nix
+            ./linode/configuration.nix
+          ];
+        };
+        resolute = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./shared.nix
+            ./resolute/configuration.nix
+          ];
+        };
+      };
+    };
 }
--- modules/sops.nix
+++ modules/sops.nix
@@ -1,14 +1,22 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  inputs,
+  ...
+}:
 {
   environment.systemPackages = [ pkgs.sops ];
-  sops = {
-    defaultSopsFile = ../secrets.yaml;
-    validateSopsFiles = false;
+  sops =
+    let
+      secretsPath = builtins.toString inputs.mysecrets;
+    in
+    {
+      defaultSopsFile = "${secretsPath}/secrets.yaml";
+      validateSopsFiles = false;
 
-    age = {
-      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      keyFile = "/var/lib/sops-nix/key.txt";
-      generateKey = true;
+      age = {
+        sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+        keyFile = "/var/lib/sops-nix/key.txt";
+        generateKey = true;
+      };
     };
-  };
 }
