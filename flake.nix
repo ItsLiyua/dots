0d00209c5311f9a31e0971f698f6392f39a389a4
Liyua <liyua@duck.com>
2025-06-10 23:20:44 +0200

    Removed raspberry pi configuration

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "System configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -32,7 +31,6 @@
         liberty = mkSysConfig nixpkgs ./hosts/liberty;
         linode = mkSysConfig nixpkgs ./hosts/linode;
         resolute = mkSysConfig nixpkgs ./hosts/resolute;
-        rpi5-1 = mkSysConfig nixos-raspberrypi ./hosts/pi;
       };
       formatter = {
         x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
