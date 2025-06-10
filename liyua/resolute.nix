d761fee9f7069854930cd596d5702ddcf7110f62
Liyua <liyua@duck.com>
2025-06-10 03:46:58 +0200

    Added refreshrates for monitors to configuration

--- liyua/resolute.nix
+++ liyua/resolute.nix
@@ -12,10 +12,12 @@
         DP-3 = {
           width = 3440;
           height = 1440;
+          refreshRate = 144;
         };
         HDMI-A-1 = {
           width = 1920;
           height = 1080;
+          refreshRate = 60;
           pos = {
             x = -1920;
             y = 1080;
