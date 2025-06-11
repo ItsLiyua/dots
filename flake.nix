da0d4856cad8ffe4218f881776c30371782502e3
Liyua <liyua@duck.com>
2025-06-11 04:39:57 +0200

    Added override for installer

--- flake.nix
+++ flake.nix
@@ -9,6 +9,15 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
+  };
+
   outputs =
     {
       nixpkgs,
