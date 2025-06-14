340a4d182ddfe634d6d11a179cf076a03c5e228b
Liyua <liyua@duck.com>
2025-06-14 17:17:13 +0200

    Fixed syntax error

--- hosts/linode/modules/remote-build-access.nix
+++ hosts/linode/modules/remote-build-access.nix
@@ -4,5 +4,5 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtyKAptgaf6VNiQPw6GvoC8/89u5+PMsQsqa8G7R4J/ liyua@resolute"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKZZyrwb0depJAXqeoN8+q8kquwgAHz3uRssneGDtp0 liyua@liberty"
   ];
-  services.openssh.PermitRootLogin = lib.mkForce "prohibit-password";
+  services.openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password";
 }
