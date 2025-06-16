1c78e5b43a18b7138da0b62146bfda9b5fbf002e
Liyua <liyua@duck.com>
2025-06-16 23:52:59 +0200

    Removed unused ssh key

--- hosts/common/core/system/user.nix
+++ hosts/common/core/system/user.nix
@@ -21,8 +21,7 @@
           "power"
         ];
         openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtyKAptgaf6VNiQPw6GvoC8/89u5+PMsQsqa8G7R4J/ liyua@resolute"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKZZyrwb0depJAXqeoN8+q8kquwgAHz3uRssneGDtp0 liyua@liberty"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKZZyrwb0depJAXqeoN8+q8kquwgAHz3uRssneGDtp0 liyua"
         ];
       };
       root.hashedPasswordFile = config.sops.secrets."root/password".path;
