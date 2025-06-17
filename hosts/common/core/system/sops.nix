59690980c694750dc611718c1278c09dd516d71c
Liyua <liyua@duck.com>
2025-06-17 15:48:57 +0200

    Added t480 decryption key to sops

--- hosts/common/core/system/sops.nix
+++ hosts/common/core/system/sops.nix
@@ -6,9 +6,9 @@
     validateSopsFiles = true;
 
     age = {
-      # sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      # keyFile = "/var/lib/sops-nix/key.txt";
-      keyFile = "/home/liyua/.config/sops/age/keys.txt";
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      keyFile = "/var/lib/sops-nix/key.txt";
+      # keyFile = "/home/liyua/.config/sops/age/keys.txt";
       generateKey = true;
     };
   };
