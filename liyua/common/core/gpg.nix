d1a0d51b34fab065d4763464848ddb8c22819ea4
Liyua <liyua@duck.com>
2025-06-17 19:13:03 +0200

    Added auto import of secret gpg key

--- liyua/common/core/gpg.nix
+++ liyua/common/core/gpg.nix
@@ -1,9 +1,17 @@
-{ config, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   sops.secrets = {
     "liyua/gpg/private" = { };
     "liyua/gpg/public" = { };
   };
+  home.activation.import-gpg-key = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    run ${pkgs.gnupg}/bin/gpg --import "${config.sops.secrets."liyua/gpg/private".path}" $VERBOSE_ARG
+  '';
   programs.gpg = {
     enable = true;
     publicKeys = [
