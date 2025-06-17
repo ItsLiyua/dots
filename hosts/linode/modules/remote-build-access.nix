ba13736ea2ae3a219ff6f36f45f72db937da2fd8
Liyua <liyua@duck.com>
2025-06-17 19:48:17 +0200

    Changed remote build access ssh key

--- hosts/linode/modules/remote-build-access.nix
+++ hosts/linode/modules/remote-build-access.nix
@@ -1,8 +1,7 @@
 { lib, ... }:
 {
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtyKAptgaf6VNiQPw6GvoC8/89u5+PMsQsqa8G7R4J/ liyua@resolute"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKZZyrwb0depJAXqeoN8+q8kquwgAHz3uRssneGDtp0 liyua@liberty"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIIQcmoKJxqALKZJIFwuM0mcT0EPlkvlMaGXSEXb0zyd liyua"
   ];
   services.openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password";
 }
