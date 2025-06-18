735f79e7da5a835e66a7644ffae8cad647876e4d
Liyua <liyua@duck.com>
2025-06-18 15:23:52 +0200

    Store optimization

--- hosts/common/core/system/nix.nix
+++ hosts/common/core/system/nix.nix
@@ -1,12 +1,17 @@
 {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-    "pipe-operators"
-  ];
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "pipe-operators"
+      ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };
 }
