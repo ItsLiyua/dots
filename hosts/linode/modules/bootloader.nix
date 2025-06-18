489781118ac7909975e46e81cfc2b34ba3164d89
Liyua <liyua@duck.com>
2025-06-18 13:29:07 +0200

    Sturctured linode config

---
+++ hosts/linode/modules/bootloader.nix
@@ -0,0 +1,18 @@
+{
+  boot = {
+    kernelParams = [ "console=ttyS0,19200n8" ];
+    loader = {
+      timeout = 10;
+      grub = {
+        enable = true;
+        device = "/dev/sda";
+        forceInstall = true;
+        extraConfig = ''
+          serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
+          terminal_input serial;
+          terminal_output serial
+        '';
+      };
+    };
+  };
+}
