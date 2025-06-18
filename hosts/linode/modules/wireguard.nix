5c39b8c60e039b5ff2a94f56debd824a5f2abf24
Liyua <liyua@duck.com>
2025-06-18 16:36:42 +0200

    Moved the wireguard server to linode

--- hosts/linode/modules/wireguard.nix
+++ hosts/linode/modules/wireguard.nix
@@ -1,19 +1,32 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   sops.secrets."wireguard/linode" = { };
 
   networking = {
+    # WireGuard server
+    nat = {
+      enable = true;
+      externalInterface = "eth0";
+      internalInterfaces = [ "wg0" ];
+    };
     firewall.allowedUDPPorts = [ 51820 ];
     wireguard.interfaces.wg0 = {
-      ips = [ "10.15.0.2/24" ];
+      ips = [ "10.15.0.1/24" ];
       listenPort = 51820;
-      privateKeyFile = config.sops.secrets."wireguard/linode".path;
+
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      privateKeyFile = config.sops.secrets."wireguard/rpi5-2".path;
+
       peers = [
         {
-          publicKey = "JzFAa30tHG5Z+mVu1GZEeXGS18FQOfoNlDqBNNcb1iw=";
-          allowedIPs = [ "10.15.0.0/24" ];
-          endpoint = builtins.readFile config.sops.secrets."publicIP".path;
-          persistentKeepalive = 25;
+          publicKey = "cOa8ACs07xdE+C7H3O/+2tA7BKfIIHaojz80WZbazlM=";
+          allowedIPs = [ "10.15.0.2/32" ];
         }
       ];
     };
--- hosts/pi/rpi5-2.nix
+++ hosts/pi/rpi5-2.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 {
   imports = [ ./modules ];
 
@@ -6,30 +6,17 @@
 
   networking = {
     hostName = "rpi5-2";
-    # WireGuard server
-    nat = {
-      enable = true;
-      externalInterface = "eth0";
-      internalInterfaces = [ "wg0" ];
-    };
     firewall.allowedUDPPorts = [ 51820 ];
     wireguard.interfaces.wg0 = {
-      ips = [ "10.15.0.1/24" ];
+      ips = [ "10.15.0.2/24" ];
       listenPort = 51820;
-
-      postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-      '';
-      postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-      '';
-
-      privateKeyFile = config.sops.secrets."wireguard/rpi5-2".path;
-
+      privateKeyFile = config.sops.secrets."wireguard/linode".path;
       peers = [
         {
-          publicKey = "cOa8ACs07xdE+C7H3O/+2tA7BKfIIHaojz80WZbazlM=";
-          allowedIPs = [ "10.15.0.2/32" ];
+          publicKey = "JzFAa30tHG5Z+mVu1GZEeXGS18FQOfoNlDqBNNcb1iw=";
+          allowedIPs = [ "10.15.0.0/24" ];
+          endpoint = "172.105.73.145:51820";
+          persistentKeepalive = 25;
         }
       ];
     };
