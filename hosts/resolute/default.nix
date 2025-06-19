78e2b766712792fee15460ebb50f2c5df445c117
Liyua <liyua@duck.com>
2025-06-19 19:14:47 +0200

    Added firewall rules for testing

--- hosts/resolute/default.nix
+++ hosts/resolute/default.nix
@@ -34,6 +34,8 @@
       swapescape.enable = true;
     };
     networking.hostName = "resolute";
+    networking.firewall.allowedTCPPorts = [ 25566 ];
+    networking.firewall.allowedUDPPorts = [ 25566 ];
     console.keyMap = "us";
 
     system.stateVersion = "25.05"; # DO NOT CHANGE UNDER ANY CIRCUMSTANCES
