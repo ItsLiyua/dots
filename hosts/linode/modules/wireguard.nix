eb93917a6e72543f0fd8db6f5f1efb8a8b7ce9ac
Liyua <liyua@duck.com>
2025-06-19 23:04:26 +0200

    Updated wireguard configs for resolute and linode

--- hosts/linode/modules/wireguard.nix
+++ hosts/linode/modules/wireguard.nix
@@ -1,38 +1,41 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 {
   sops.secrets."wireguard/linode".group = "systemd-network";
 
   networking = {
-    # WireGuard server
-    nat = {
-      enable = true;
-      externalInterface = "eth0";
-      internalInterfaces = [ "wg0" ];
-    };
     firewall.allowedUDPPorts = [ 51820 ];
-    wireguard.interfaces.wg0 = {
-      ips = [ "10.15.0.1/24" ];
-      listenPort = 51820;
-
-      postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-      '';
-      postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-      '';
-
-      privateKeyFile = config.sops.secrets."wireguard/linode".path;
-
-      peers = [
+  };
+  systemd.network = {
+    netdevs."50-wg0" = {
+      netdevConfig = {
+        Kind = "wireguard";
+        Name = "wg0";
+        MTUBytes = "1300";
+      };
+      wireguardConfig = {
+        PrivateKeyFile = config.sops.secrets."wireguard/linode".path;
+        ListenPort = 51820;
+        RouteTable = "main";
+      };
+      wireguardPeers = [
         {
-          publicKey = "JzFAa30tHG5Z+mVu1GZEeXGS18FQOfoNlDqBNNcb1iw=";
-          allowedIPs = [ "10.15.0.2/32" ];
+          PublicKey = "JzFAa30tHG5Z+mVu1GZEeXGS18FQOfoNlDqBNNcb1iw=";
+          AllowedIPs = [ "10.15.0.2" ]; # /32
         }
         {
-          publicKey = "PCFtL6M/vfrxFFVSXAi7ascCUFBrM9i8PhLLuTUDeyA=";
-          allowedIPs = [ "10.15.0.3/32" ];
+          PublicKey = "PCFtL6M/vfrxFFVSXAi7ascCUFBrM9i8PhLLuTUDeyA=";
+          AllowedIPs = [ "10.15.0.3" ]; # /32
         }
       ];
+    };
+
+    networks.wg0 = {
+      matchConfig.Name = "wg0";
+      address = [ "10.15.0.1/24" ];
+      networkConfig = {
+        IPMasquerade = "ipv4";
+        IPForward = true;
+      };
     };
   };
 }
--- hosts/resolute/modules/wireguard.nix
+++ hosts/resolute/modules/wireguard.nix
@@ -1,20 +1,42 @@
 { config, ... }:
 {
   sops.secrets."wireguard/resolute".group = "systemd-network";
-  networking = {
-    firewall.allowedUDPPorts = [ 51820 ];
-    wireguard.interfaces.wg0 = {
-      ips = [ "10.15.0.3/24" ];
-      listenPort = 51820;
-      privateKeyFile = config.sops.secrets."wireguard/resolute".path;
-      peers = [
+  boot.kernelModules = [ "wireguard" ];
+  systemd.network = {
+    netdevs."10-wg0" = {
+      netdevConfig = {
+        Kind = "wireguard";
+        Name = "wg0";
+        MTUBytes = "1300";
+      };
+      wireguardConfig = {
+        PrivateKeyFile = config.sops.secrets."wireguard/resolute".path;
+        ListenPort = 51820;
+      };
+      wireguardPeers = [
         {
-          publicKey = "cOa8ACs07xdE+C7H3O/+2tA7BKfIIHaojz80WZbazlM=";
-          allowedIPs = [ "10.15.0.0/24" ];
-          endpoint = "172.105.73.145:51820";
-          persistentKeepalive = 25;
+          PublicKey = "cOa8ACs07xdE+C7H3O/+2tA7BKfIIHaojz80WZbazlM=";
+          AllowedIPs = [ "10.15.0.1/24" ];
+          Endpoint = "172.105.73.145:51820";
+          PersistentKeepalive = 25;
         }
       ];
+    };
+    networks.wg0 = {
+      matchConfig.Name = "wg0";
+      address = [
+        "10.15.0.3/24"
+        "fc00::3/120"
+        "fe80::3/64"
+      ];
+      DHCP = "no";
+      dns = [ "fc00::53" ];
+      ntp = [ "fc00::123" ];
+      gateway = [
+        "fc00::1"
+        "10.15.0.1"
+      ];
+      networkConfig.IPv6AcceptRA = false;
     };
   };
 }
