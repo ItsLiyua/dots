9087204af5159164e1a7989eef9f68dae0c87153
Liyua <liyua@duck.com>
2025-06-19 03:29:43 +0200

    Removed nginx again

--- hosts/linode/default.nix
+++ hosts/linode/default.nix
@@ -4,7 +4,6 @@
     ./modules/remote-build-access.nix
     ./modules/bootloader.nix
     ./modules/network.nix
-    ./modules/nginx.nix
     ./modules/linode-utils.nix
     ./modules/wireguard.nix
     ./hardware-configuration.nix
