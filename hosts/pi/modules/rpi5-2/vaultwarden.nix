b19c30b67ef0e260e2a23bdd9a52a4a44eec5861
Liyua <liyua@duck.com>
2025-06-20 15:18:57 +0200

    Vaultwarden now owner of env file

--- hosts/pi/modules/rpi5-2/vaultwarden.nix
+++ hosts/pi/modules/rpi5-2/vaultwarden.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
-  sops.secrets."vaultwarden/envFile" = { };
+  sops.secrets."vaultwarden/envFile".owner = "vaultwarden";
   services.vaultwarden = {
     enable = true;
     environmentFile = config.sops.secrets."vaultwarden/envFile".path;
