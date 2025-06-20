a33c33538b4f4b975299e57656605c94c9946837
Liyua <liyua@duck.com>
2025-06-20 22:06:56 +0200

    Fixed velocity and disabled bstats

--- hosts/linode/modules/velocity.nix
+++ hosts/linode/modules/velocity.nix
@@ -9,15 +9,21 @@
       enable = true;
       package = pkgs.velocityServers.velocity;
       stopCommand = "end";
-      files."velocity.toml".value = {
-        config-version = "2.5";
-        bind = "0.0.0.0:25565";
-        motd = "Velocity Procy";
-        online-mode = true;
-        servers.test = "10.15.0.2:25565";
-        forced-hosts.test = [ "test" ];
-        player-info-forwarding-mode = "modern";
-        forwarding-secret-file = config.sops.secrets.velocity.path;
+      files = {
+        "velocity.toml".value = {
+          config-version = "2.5";
+          bind = "0.0.0.0:25565";
+          motd = "Velocity Procy";
+          online-mode = true;
+          servers = {
+            test = "10.15.0.2:25565";
+            try = [ "test" ];
+          };
+          forced-hosts.test = [ "test" ];
+          player-info-forwarding-mode = "modern";
+          forwarding-secret-file = config.sops.secrets.velocity.path;
+        };
+        "plugins/bStats/config.txt".value.enabled = false;
       };
     };
   };
