e9d0c05224f9173458370d0c7244501954e231f7
Liyua <liyua@duck.com>
2025-06-21 20:00:17 +0200

    Changed rocket address

--- hosts/pi/modules/rpi5-1/vaultwarden.nix
+++ hosts/pi/modules/rpi5-1/vaultwarden.nix
@@ -5,7 +5,7 @@
     config = {
       DOMAIN = "https://vault.liyua.moe";
       SIGNUPS_ALLOWED = false;
-      ROCKET_ADDRESS = "127.0.0.1";
+      ROCKET_ADDRESS = "10.15.0.3";
       ROCKET_PORT = 8222;
       EXTENDED_LOGGING = true;
       LOG_LEVEL = "warn";
