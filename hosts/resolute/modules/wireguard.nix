d8a5a228db3013181c0803c93e14d444b4716640
Liyua <liyua@duck.com>
2025-06-21 17:37:31 +0200

    Allowed interconnectivity between different wg peers and replaced wg server ip with url

--- hosts/resolute/modules/wireguard.nix
+++ hosts/resolute/modules/wireguard.nix
@@ -22,9 +22,9 @@
         {
           # Linode
           PublicKey = "cOa8ACs07xdE+C7H3O/+2tA7BKfIIHaojz80WZbazlM=";
-          AllowedIPs = [ "10.15.0.1" ];
+          AllowedIPs = [ "10.15.0.1/24" ];
           PersistentKeepalive = 25;
-          Endpoint = [ "172.105.73.145:51820" ];
+          Endpoint = [ "liyua.moe:51820" ];
         }
       ];
     };
