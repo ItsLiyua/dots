898a4d5a847c956dd6ddf23e570842d9b61a7c38
Liyua <liyua@duck.com>
2025-06-24 01:39:27 +0200

    Removed remote build access from linode

--- hosts/linode/default.nix
+++ hosts/linode/default.nix
@@ -1,7 +1,6 @@
 {
   imports = [
     ./modules/disko.nix
-    ./modules/remote-build-access.nix
     ./modules/bootloader.nix
     ./modules/network.nix
     ./modules/linode-utils.nix
--- hosts/linode/modules/remote-build-access.nix
+++
@@ -1,5 +0,0 @@
-{
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIIQcmoKJxqALKZJIFwuM0mcT0EPlkvlMaGXSEXb0zyd liyua"
-  ];
-}
