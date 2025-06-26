2a5ed836b0c256d1abd1587d54ec239408b8f0a7
Liyua <liyua@duck.com>
2025-06-26 23:50:06 +0200

    Configured niri outputs for t480

--- liyua/common/optional/desktop/niri/default.nix
+++ liyua/common/optional/desktop/niri/default.nix
@@ -10,8 +10,30 @@
       enable = true;
       settings = {
         binds = {
-          "Mod+A".action.spawn = "${pkgs.wofi}/bin/wofi --show drun";
+          "Mod+A".action.spawn = [
+            "${pkgs.wofi}/bin/wofi"
+            "--show"
+            "drun"
+          ];
           "Mod+T".action.spawn = "${pkgs.foot}/bin/foot";
+          "Mod+F".action.spawn = "firefox";
+
+        };
+        input = {
+          keyboard.xkb.layout = "de";
+          touchpad = {
+
+          };
+          mouse.accel-profile = "flat";
+          focus-follows-mouse.enable = true;
+        };
+        outputs."eDP-1" = {
+          scale = 1;
+          mode = {
+            height = 1920;
+            width = 1080;
+            refresh = 60.0;
+          };
         };
       };
     };
