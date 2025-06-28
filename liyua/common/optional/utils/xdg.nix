b8ad1c402c2548ccc45f6bc5232629090a2d9f25
Liyua <liyua@duck.com>
2025-06-28 23:06:20 +0200

    Switched back to hyprland

--- liyua/common/optional/utils/xdg.nix
+++ liyua/common/optional/utils/xdg.nix
@@ -6,22 +6,22 @@
 }:
 {
   xdg = lib.mkIf config.liyua.xdg.enable {
-    # portal = {
-    #   enable = true;
-    #   extraPortals = [
-    #     pkgs.xdg-desktop-portal-gtk
-    #   ];
-    #   config = {
-    #     hyprland.preferred = [
-    #       "hyprland"
-    #       "gtk"
-    #     ];
-    #     niri.preferred = [ "gtk" ];
-    #   };
-    # };
-    # userDirs = {
-    #   enable = true;
-    #   createDirectories = true;
-    # };
+    portal = {
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+      ];
+      config = {
+        hyprland.preferred = [
+          "hyprland"
+          "gtk"
+        ];
+        niri.preferred = [ "gtk" ];
+      };
+    };
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
   };
 }
