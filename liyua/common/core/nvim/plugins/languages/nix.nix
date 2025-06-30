b6ec0744df41bfd994e7ebe7ddd6c1d5c2ce6447
Liyua <liyua@duck.com>
2025-06-30 03:05:55 +0200

    Actually fixed lsps this time

--- liyua/common/core/nvim/plugins/languages/nix.nix
+++ liyua/common/core/nvim/plugins/languages/nix.nix
@@ -1,6 +1,5 @@
 {
   config,
-  lib,
   pkgs,
   nil_ls,
   ...
@@ -9,7 +8,8 @@
   programs.nvf.settings.vim.languages.nix = {
     enable = true;
     format.type = "nixfmt";
-    lsp = lib.mkIf config.liyua.cli.nvim.lsp {
+    lsp = {
+      enable = config.liyua.cli.nvim.lsp;
       package = nil_ls.packages.${pkgs.system}.nil;
       server = "nil";
     };
