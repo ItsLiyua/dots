ce063f35e4a0a5535dc1ba84eefea2e8dfda6521
Liyua <liyua@duck.com>
2025-07-02 03:56:43 +0200

    Added steam sale widget and removed calendar as it is not necessary

--- hosts/pi/rpi5-1/glance.nix
+++ hosts/pi/rpi5-1/glance.nix
@@ -37,10 +37,6 @@
                   ];
                 }
                 {
-                  type = "calendar";
-                  first-day-of-week = "monday";
-                }
-                {
                   type = "weather";
                   location = "Hamburg, Germany";
                   units = "metric";
@@ -134,15 +130,29 @@
                       ];
                     }
                     {
-                      type = "twitch-channels";
-                      channels = [
-                        "theprimeagen"
-                        "j_blow"
-                        "piratesoftware"
-                        "cohhcarnage"
-                        "christitustech"
-                        "EJ_SA"
-                      ];
+                      type = "custom-api";
+                      title = "Steam Specials";
+                      cache = "12h";
+                      url = "https://store.steampowered.com/api/featuredcategories?cc=de";
+                      template = ''
+                        <ul class="list list-gap-10 collapsible-container" data-collapse-after="5">
+                        {{ range .JSON.Array "specials.items" }}
+                          {{ $header := .String "header_image" }}
+                          {{ $urlPrefix := "https://store.steampowered.com/sub/" }}
+                          {{ if findMatch "/steam/apps/" $header }}
+                            {{ $urlPrefix = "https://store.steampowered.com/app/" }}
+                          {{ end }}
+                          <li>
+                            <a class="size-h4 color-highlight block text-truncate" href="{{ $urlPrefix }}{{ .Int "id" }}/">{{ .String "name" }}</a>
+                            <ul class="list-horizontal-text">
+                              <li>{{ .Int "final_price" | toFloat | mul 0.01 | printf "$%.2f" }}</li>
+                              {{ $discount := .Int "discount_percent" }}
+                              <li{{ if ge $discount 40 }} class="color-positive"{{ end }}>{{ $discount }}% off</li>
+                            </ul>
+                          </li>
+                        {{ end }}
+                        </ul>
+                      '';
                     }
                   ];
                 }
