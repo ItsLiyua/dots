646e6254ff2c739fb4cfabf2509bd7e634929c0e
Liyua <liyua@duck.com>
2025-07-15 23:34:14 +0200

    Changed nfs mount name

--- hosts/common/optional/system/nfs-mounts.nix
+++ hosts/common/optional/system/nfs-mounts.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }:
 {
   fileSystems."/mnt/music" = lib.mkIf config.liyua.nfs.music.enable {
-    device = "10.15.0.3:/var/exports/music";
+    device = "10.15.0.3:/music";
     fsType = "nfs";
   };
 }
