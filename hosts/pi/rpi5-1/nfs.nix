f32ae0c74f1677d7c84f5a2149e702c2451dd3c0
Liyua <liyua@duck.com>
2025-07-15 23:13:07 +0200

    Added nfs ports to firewall

--- hosts/pi/rpi5-1/nfs.nix
+++ hosts/pi/rpi5-1/nfs.nix
@@ -1,10 +1,21 @@
 {
+  networking.firewall.allowedTCPPorts = [
+    111
+    2049
+    4000
+    4001
+    4002
+    20048
+  ];
   services.nfs.server =
     let
       exportRoot = "/var/exports";
     in
     {
       enable = true;
+      lockdPort = 4001;
+      mountdPort = 4002;
+      statdPort = 4000;
       createMountPoints = true;
       exports = ''
         ${exportRoot}/music 10.15.0.0/24(rw,no_subtree_check)
