1b4f4a64b47ace4a6815fbdf0f37cfdac8d33f57
Liyua <liyua@duck.com>
2025-07-19 18:19:25 +0200

    Moved one nh env var back

--- hosts/common/core/utils/nh.nix
+++ hosts/common/core/utils/nh.nix
@@ -1,15 +1,10 @@
 {
-  config = {
-    environment.sessionVariables = {
-      NH_OS_FLAKE = "/etc/nixos";
-      NH_HOME_FLAKE = "$HOME/.config/home-manager";
-    };
-    programs.nh = {
-      enable = true;
-      clean = {
-        enable = false;
-        extraArgs = "--keep-since 4d --keep 5";
-      };
+  environment.sessionVariables.NH_OS_FLAKE = "/etc/nixos";
+  programs.nh = {
+    enable = true;
+    clean = {
+      enable = false;
+      extraArgs = "--keep-since 4d --keep 5";
     };
   };
 }
