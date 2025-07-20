a801e479c3339f829d9d768343cb25cd2a0a0c32
Liyua <liyua@duck.com>
2025-07-20 16:54:29 +0200

    Refactored some stuff in regards to the greeter

--- hosts/common/optional/gui/greeter.nix
+++ hosts/common/optional/gui/greeter.nix
@@ -4,18 +4,17 @@
   pkgs,
   ...
 }:
-{
-  services.greetd =
-    with config.liyua.ui.greeter;
-    lib.mkIf enable {
-      enable = true;
-      settings = {
-        default_session = {
-          command = ''${pkgs.greetd.tuigreet}/bin/tuigreet -tr --asterisks --asterisks-char "*" ${
-            if text != null then ''-g "${text}"'' else ""
-          } --user-menu --window-padding 2 --theme "text=white;container=black;border=red;greet=white;prompt=red;input=white;action=white" --cmd "${launchCommand}"'';
-          user = "greeter";
-        };
+with config.liyua.ui.greeter;
+lib.mkIf enable {
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = ''${pkgs.greetd.tuigreet}/bin/tuigreet -tr --asterisks --asterisks-char "*" ${
+          if text != null then ''-g "${text}"'' else ""
+        } --user-menu --window-padding 2 --theme "text=white;container=black;border=red;greet=white;prompt=red;input=white;action=white" --cmd "${launchCommand}"'';
+        user = "greeter";
       };
     };
+  };
 }
