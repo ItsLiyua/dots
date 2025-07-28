8a6503291ad974d8a5036575610acd7025deac89
Liyua <liyua@duck.com>
2025-07-28 21:46:36 +0200

    Added nameservers

--- hosts/common/core/system/network.nix
+++ hosts/common/core/system/network.nix
@@ -2,6 +2,7 @@
 {
   networking = {
     useNetworkd = true;
+    enableIPv6 = true;
     inherit (config.liyua.network) usePredictableInterfaceNames;
     firewall.enable = true;
     wireless.iwd = lib.mkIf config.liyua.network.wireless.enable {
@@ -11,6 +12,10 @@
         Settings.AutoConnect = true;
       };
     };
+    nameservers = [
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
+    ];
   };
   systemd.network.enable = true;
 }
