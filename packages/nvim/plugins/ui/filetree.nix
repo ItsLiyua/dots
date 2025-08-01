f42f3dbecae3914b6c6689516aa7000d4ca42279
Liyua <liyua@duck.com>
2025-08-01 22:35:31 +0200

    Added common nvim options

--- packages/nvim/plugins/ui/filetree.nix
+++ packages/nvim/plugins/ui/filetree.nix
@@ -3,16 +3,28 @@
   vim = {
     filetree.neo-tree = {
       enable = true;
-      setupOpts.event_handlers = [
-        {
-          event = "file_open_requested";
-          handler = lib.mkLuaInline ''
-            function()
-              require("neo-tree.command").execute({ action = "close" })
-            end
-          '';
-        }
-      ];
+      setupOpts = {
+        close_if_last_window = true;
+        filesystem = {
+          filtered_items = {
+            visible = true;
+            hide_dotfiles = false;
+            hide_gitignored = false;
+            hide_hidden = false;
+          };
+          follow_current_file.enabled = true;
+        };
+        event_handlers = [
+          {
+            event = "file_open_requested";
+            handler = lib.mkLuaInline ''
+              function()
+                require("neo-tree.command").execute({ action = "close" })
+              end
+            '';
+          }
+        ];
+      };
     };
     keymaps = [
       {
