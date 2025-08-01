2e0f35baaaaa19f7247b46d7a6c7089e4580832e
Liyua <liyua@duck.com>
2025-08-01 21:45:01 +0200

    Changed the secret path back

--- liyua/common/core/sops.nix
+++ liyua/common/core/sops.nix
@@ -1,6 +1,6 @@
 {config, ...}: {
   sops = {
-    defaultSymlinkPath = "${config.xdg.configHome}/sops-nix-new/secrets";
+    defaultSymlinkPath = "${config.xdg.configHome}/sops-nix/secrets";
     defaultSopsFile = ../../../secrets/liyua.yaml;
     validateSopsFiles = true;
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
