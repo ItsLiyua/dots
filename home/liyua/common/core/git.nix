764c4398526b7a0911f2c9701cdfa6954b81055e
Liyua <liyua@duck.com>
2025-08-07 03:35:17 +0200

    Do you even sign bro?

--- home/liyua/common/core/git.nix
+++ home/liyua/common/core/git.nix
@@ -16,7 +16,7 @@
       user.signingkey = "${config.home.homeDirectory}/.ssh/id_yubikey";
     };
   };
-  home.file.".ssh/allowed_signers".value = ''
+  home.file.".ssh/allowed_signers".text = ''
     liyua@nano sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIP8pXb07NpVYuq3VyCnrI7DAZFvq2wDYrUehggKTkUl9AAAABHNzaDo= liyua@nano
     liyua@nfc sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICdngkr57QIPqK6Lm4235y61BUG9jjU1fJioVZRWb2zIAAAABHNzaDo= liyua@nfc
   '';
