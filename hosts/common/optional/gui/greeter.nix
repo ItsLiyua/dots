464bff19286401a7e577a0e17a393d05b4eb2008
Liyua <liyua@liyua.moe>
2025-08-08 02:26:21 +0200

    Fixed renamed tuigreet package

--- hosts/common/optional/gui/greeter.nix
+++ hosts/common/optional/gui/greeter.nix
@@ -10,7 +10,7 @@ lib.mkIf enable {
     enable = true;
     settings = {
       default_session = {
-        command = ''${pkgs.greetd.tuigreet}/bin/tuigreet -tr --asterisks --asterisks-char "*" ${
+        command = ''${pkgs.tuigreet}/bin/tuigreet -tr --asterisks --asterisks-char "*" ${
           if text != null then ''-g "${text}"'' else ""
         } --user-menu --window-padding 2 --theme "text=white;container=black;border=red;greet=white;prompt=red;input=white;action=white" --cmd "${launchCommand}"'';
         # command = ''${arasaka-greeter.packages.${pkgs.system}.default}/bin/launch-arasaka-greeter'';
