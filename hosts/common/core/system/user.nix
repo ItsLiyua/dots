5972fc33f8a4f7d54abe604e666c248d159d56a1
Liyua <liyua@liyua.moe>
2025-08-08 02:47:57 +0200

    Updated nano key

--- hosts/common/core/system/user.nix
+++ hosts/common/core/system/user.nix
@@ -22,7 +22,7 @@
         ];
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIIQcmoKJxqALKZJIFwuM0mcT0EPlkvlMaGXSEXb0zyd liyua"
-          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIN0+B3yM7FqDHXq/F99ZAvnM3Vap1u9+g59NXfDk34lQAAAABHNzaDo= liyua@nano"
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIM6+1cR0P5EjO26/+LzJODBxOJzR9I3m0tNM6c5J/OYVAAAABHNzaDo= liyua@nano"
           "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIL7rfMuc1NerL/NTnkABcwIuZNSlUz9AnowvIUDT6LN4AAAABHNzaDo= liyua@nfc"
         ];
       };
