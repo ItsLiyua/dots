14cbc3fb4c0db4266612d96ff89eae6c826166fb
Liyua <liyua@liyua.moe>
2025-08-29 20:28:06 +0200

    Fixed issue with config.txt declaration

--- hosts/pi/rpi5-1/jellyfin.nix
+++ hosts/pi/rpi5-1/jellyfin.nix
@@ -1,6 +1,6 @@
 {
   users.users.jellyfin.extraGroups = [ "video" ];
-  hardware.raspberry-pi.config.all.gpu_mem = "2048";
+  hardware.raspberry-pi.extraConfig.all.gpu_mem = "2048";
   services.jellyfin = {
     enable = true;
     openFirewall = true;
