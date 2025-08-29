a80d882f2c229e993534e425288b58de003a8fae
Liyua <liyua@liyua.moe>
2025-08-29 20:28:43 +0200

    Fixed missing word

--- hosts/pi/rpi5-1/jellyfin.nix
+++ hosts/pi/rpi5-1/jellyfin.nix
@@ -1,6 +1,6 @@
 {
   users.users.jellyfin.extraGroups = [ "video" ];
-  hardware.raspberry-pi.extraConfig.all.gpu_mem = "2048";
+  hardware.raspberry-pi.config.extraConfig.all.gpu_mem = "2048";
   services.jellyfin = {
     enable = true;
     openFirewall = true;
