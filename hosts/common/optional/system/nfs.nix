fca1c2dd3e80f2c5eba1be2d149423ddda0ab099
Liyua <liyua@liyua.moe>
2025-09-03 18:37:59 +0200

    Fixed missing brackets

--- hosts/common/optional/system/nfs.nix
+++ hosts/common/optional/system/nfs.nix
@@ -8,27 +8,31 @@
       movies.enable = lib.mkEnableOption "Movie Share";
     };
   };
-  config = lib.mkIf config.liyua.nfs.enable {
-    fileSystems =
-      let
-        mountOpts = {
-          fsType = "nfs";
-          options = [
-            "nfsvers=4.2"
-            "nofail"
-          ];
+  config =
+    let
+      cfg = config.liyua.nfs;
+    in
+    lib.mkIf cfg.enable {
+      fileSystems =
+        let
+          mountOpts = {
+            fsType = "nfs";
+            options = [
+              "nfsvers=4.2"
+              "nofail"
+            ];
+          };
+        in
+        {
+          "/mnt/nfs/anime" = lib.mkIf cfg.drives.anime.enable (
+            mountOpts // { device = "rpi5-1.local:/export/anime"; }
+          );
+          "/mnt/nfs/movies" = lib.mkIf cfg.drives.movies.enable (
+            mountOpts // { device = "rpi5-1.local:/export/anime"; }
+          );
+          "/mnt/nfs/music" = lib.mkIf cfg.drives.music.enable (
+            mountOpts // { device = "rpi5-1.local:/export/anime"; }
+          );
         };
-      in
-      {
-        "/mnt/nfs/anime" = lib.mkIf config.liyua.nfs.drives.anime.enable mountOpts // {
-          device = "rpi5-1.local:/export/anime";
-        };
-        "/mnt/nfs/movies" = lib.mkIf config.liyua.nfs.drives.movies.enable mountOpts // {
-          device = "rpi5-1.local:/export/anime";
-        };
-        "/mnt/nfs/music" = lib.mkIf config.liyua.nfs.drives.music.enable mountOpts // {
-          device = "rpi5-1.local:/export/anime";
-        };
-      };
-  };
+    };
 }
