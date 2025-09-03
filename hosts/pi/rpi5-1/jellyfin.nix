0d5fb2c330cf915413b87b34060ba6ed66478fed
Liyua <liyua@liyua.moe>
2025-09-03 16:03:06 +0200

    Removed unused stuff from jellyfin

--- hosts/pi/rpi5-1/jellyfin.nix
+++ hosts/pi/rpi5-1/jellyfin.nix
@@ -1,9 +1,4 @@
 {
-  users.users.jellyfin.extraGroups = [ "video" ];
-  hardware.raspberry-pi.config.all.options.gpu_mem = {
-    enable = true;
-    value = "2048";
-  };
   services.jellyfin = {
     enable = true;
     openFirewall = true;
