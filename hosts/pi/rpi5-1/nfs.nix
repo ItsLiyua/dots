7e96f49a066fd5b870b2c6bcb15b30d643713b26
Liyua <liyua@liyua.moe>
2025-09-03 18:28:24 +0200

    Changed nfs exports

--- hosts/pi/rpi5-1/nfs.nix
+++ hosts/pi/rpi5-1/nfs.nix
@@ -8,7 +8,9 @@ lib.mkIf enable {
   services.nfs.server = {
     enable = true;
     exports = ''
-      /export 10.100.0.0/24(rw,nohide,no_subtree_check) 127.0.0.0/24(rw,nohide,no_subtree_check)
+      /export/music   10.100.0.0/24(rw,nohide,no_subtree_check,all_squash)
+      /export/movies  10.100.0.0/24(rw,nohide,no_subtree_check,all_squash) 
+      /export/anime   10.100.0.0/24(rw,nohide,no_subtree_check,all_squash)
     '';
   };
 }
