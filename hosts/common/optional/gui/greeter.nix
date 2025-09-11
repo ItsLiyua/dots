8d7d692481242980884499caf0833092cd766bb0
Liyua <liyua@liyua.moe>
2025-09-11 23:08:10 +0200

    Fixed dconf error

--- hosts/common/optional/gui/greeter.nix
+++ hosts/common/optional/gui/greeter.nix
@@ -18,17 +18,22 @@
       default = null;
     };
   };
-  config.services.greetd =
-    with config.liyua.ui.greeter;
-    lib.mkIf enable {
-      enable = true;
-      settings = {
-        default_session = {
-          command = ''${pkgs.tuigreet}/bin/tuigreet -tr --asterisks --asterisks-char "*" ${
-            if text != null then ''-g "${text}"'' else ""
-          } --user-menu --window-padding 2 --theme "text=white;container=black;border=red;greet=white;prompt=red;input=white;action=white" --cmd "${launchCommand}"'';
-          # command = ''${arasaka-greeter.packages.${pkgs.system}.default}/bin/launch-arasaka-greeter'';
-          user = "greeter";
+  config =
+    let
+      cfg = config.liyua.ui.greeter;
+    in
+    lib.mkIf cfg.enable {
+      programs.dconf.enable = true; # Required by some desktop utils in the home manager config.
+      services.greetd = {
+        enable = true;
+        settings = {
+          default_session = {
+            command = ''${pkgs.tuigreet}/bin/tuigreet -tr --asterisks --asterisks-char "*" ${
+              if cfg.text != null then ''-g "${cfg.text}"'' else ""
+            } --user-menu --window-padding 2 --theme "text=white;container=black;border=red;greet=white;prompt=red;input=white;action=white" --cmd "${cfg.launchCommand}"'';
+            # command = ''${arasaka-greeter.packages.${pkgs.system}.default}/bin/launch-arasaka-greeter'';
+            user = "greeter";
+          };
         };
       };
     };
