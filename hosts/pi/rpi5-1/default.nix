f4067bc76fd2d290ead0039673b50259d12d2644
Liyua <liyua@liyua.moe>
2025-09-25 12:57:34 +0200

    Removed rpi5-1 nfs drive

--- hosts/pi/rpi5-1/default.nix
+++ hosts/pi/rpi5-1/default.nix
@@ -4,13 +4,22 @@
     ../common
     ./glance.nix
     ./jellyfin.nix
-    ./nfs.nix
     ./radicale.nix
     ./vaultwarden.nix
   ];
-  liyua.network.wireguard = {
-    enable = true;
-    self = "rpi5-1"; # See modules/network/wireguard.nix
+  liyua = {
+    network.wireguard = {
+      enable = true;
+      self = "rpi5-1"; # See modules/network/wireguard.nix
+    };
+    nfs = {
+      enable = true;
+      drives = {
+        anime.enable = true;
+        movies.enable = true;
+        music.enable = true;
+      };
+    };
   };
   networking.hostName = "rpi5-1";
 }
