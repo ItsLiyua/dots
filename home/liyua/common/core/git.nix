e5ffc4e68840b4a5a197fc4c16ada222e1ebf4e0
Liyua <liyua@liyua.moe>
2025-09-30 14:47:02 +0200

    Fixed wrong signing key being used

--- home/liyua/common/core/git.nix
+++ home/liyua/common/core/git.nix
@@ -23,8 +23,11 @@
       includes = [
         {
           condition = "gitdir:~/Documents/Uni/";
-          contents.user.signingkey = "${config.home.homeDirectory}/.ssh/id_uni_yubikey";
           path = config.sops.secrets."liyua/uni-gitconfig".path;
+        }
+        {
+          condition = "gitdir:~/Documents/Uni/";
+          contents.user.signingkey = "${config.home.homeDirectory}/.ssh/id_uni_yubikey";
         }
       ];
     };
