b14655680c6a2291eac82bcb8eaf5655760b3439
Liyua <liyua@liyua.moe>
2025-10-02 12:11:35 +0200

    Added 0.0.0.0 as a listening address

--- hosts/pi/eros/grafana.nix
+++ hosts/pi/eros/grafana.nix
@@ -3,7 +3,10 @@
     enable = true;
     openFirewall = true;
     settings = {
-      server.http_port = 3000;
+      server = {
+        http_addr = "0.0.0.0";
+        http_port = 3000;
+      };
     };
   };
 }
