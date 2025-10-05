a252333ae1969a58667b81a570e4f677621604e3
Liyua <liyua@liyua.moe>
2025-10-05 18:18:00 +0200

    Added default config with catppuccin colors

--- packages/desktop-shell/config.ts
+++ packages/desktop-shell/config.ts
@@ -1,4 +1,4 @@
-import { readFile } from "ags/file";
+import { readFile, writeFile } from "ags/file";
 
 interface Theme {
   base00: string;
@@ -24,7 +24,33 @@ interface Config {
   themePath: string;
 }
 
-// TODO: Enable monitoring of files to hot reload colors and so on.
-export const contents: Config = JSON.parse(
-  readFile("~/.config/desktop-shell/config.json"),
-);
+export let contents: Config = {
+  theme: {
+    base00: "#1e1e2e",
+    base01: "#181825",
+    base02: "#313244",
+    base03: "#45475a",
+    base04: "#585b70",
+    base05: "#cdd6f4",
+    base06: "#f5e0dc",
+    base07: "#b4befe",
+    base08: "#f38ba8",
+    base09: "#fab387",
+    base0A: "#f9e2af",
+    base0B: "#a6e3a1",
+    base0C: "#94e2d5",
+    base0D: "#89b4fa",
+    base0E: "#cba6f7",
+    base0F: "#f2cdcd",
+  },
+  themePath: ".cache/desktop-shell/theme.scss",
+};
+try {
+  const cfgFileContent = readFile(".config/desktop-shell/config.json");
+  contents = JSON.parse(cfgFileContent);
+} catch (e) {
+  writeFile(
+    ".config/desktop-shell/config.json",
+    JSON.stringify(contents, null, 2),
+  );
+}
