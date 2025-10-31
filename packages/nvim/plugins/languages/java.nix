54d0396187055732bac9b7b95254464563ff8498
Liyua <liyua@liyua.moe>
2025-10-31 18:06:27 +0100

    Added java formatter

--- packages/nvim/plugins/languages/java.nix
+++ packages/nvim/plugins/languages/java.nix
@@ -1,3 +1,13 @@
+{ pkgs, ... }:
 {
-  vim.languages.java.enable = true;
+  vim = {
+    languages.java.enable = true;
+    formatter.conform-nvim = {
+      enable = true;
+      setupOpts = {
+        formatters.google-java-format.command = "${pkgs.google-java-format}/bin/google-java-format";
+        formatters_by_ft.java = [ "google-java-format" ];
+      };
+    };
+  };
 }
