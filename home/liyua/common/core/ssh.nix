ef85312a468ad67d93abcd0faa5842723be20279
Liyua <liyua@liyua.moe>
2025-11-25 13:27:46 +0100

    Enabled ssh access via vpn tunnel

--- home/liyua/common/core/ssh.nix
+++ home/liyua/common/core/ssh.nix
@@ -27,6 +27,18 @@
         ] (hostname: mkCfgStdKey "${hostname}.local")
         // {
           medina = mkCfgStdKey "liyua.moe";
+          donnager = mkCfgStdKey "10.15.0.2";
+          donnager-local = mkCfgStdKey "donnager.local";
+          eros = mkCfgStdKey "10.15.0.3";
+          eros-local = mkCfgStdKey "eros.local";
+          ceres = mkCfgStdKey "10.15.0.4";
+          ceres-local = mkCfgStdKey "ceres.local";
+          razorback = mkCfgStdKey "10.15.0.5";
+          razorback-local = mkCfgStdKey "razorback.local";
+          rocinante = mkCfgStdKey "10.15.0.6";
+          rocinante-local = mkCfgStdKey "rocinante.local";
+          ganymede = mkCfgStdKey "10.15.0.7";
+          ganymede-local = mkCfgStdKey "ganymede.local";
         }
       );
     includes = [ config.sops.secrets."liyua/uni/sshconfig".path ];
