4ebe5b3a9503bbc36036b86bbb2ee54e4acd62ae
Liyua <liyua@liyua.moe>
2025-11-27 23:40:33 +0100

    Gitea WIP

--- hosts/ganymede/modules/gitea.nix
+++ hosts/ganymede/modules/gitea.nix
@@ -3,6 +3,12 @@
     enable = true;
     stateDir = "/export/shared/internal/gitea";
     lfs.enable = true;
-    settings.server.SSH_PORT = 2222;
+    settings = {
+      server = {
+        SSH_PORT = 2222;
+        DOMAIN = "";
+      };
+      # service.DISABLE_REGISTRATION = true;
+    };
   };
 }
